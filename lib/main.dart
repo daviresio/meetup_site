4f766742491d208663004022a9f90f4b529ba856
Davi Resio <daviresio@gmail.com>
2021-11-23 19:21:30 -0300

    fix ios issues

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
         textTheme: TextTheme(
           headline1: TextStyle(
             fontFamily: 'Nominee',
